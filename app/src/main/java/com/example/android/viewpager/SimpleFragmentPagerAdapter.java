a7e360af5fb0d737d55b62c4a2f9a58b8a287d26
Dmitry Kozhukhar <dna.coj@gmail.com>
2018-11-11 22:59:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/viewpager/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.viewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Bundle bundl = new Bundle();
+        MondayFragment fragment = new MondayFragment();
+        if (position == 0) {
+            bundl.putString("day","Monday");
+            fragment.setArguments(bundl);
+            return fragment;
+        } else if (position == 1){
+            bundl.putString("day","Tuesday");
+            fragment.setArguments(bundl);
+            return fragment;
+        } else if (position == 2) {
+            bundl.putString("day", "Wednesday");
+            fragment.setArguments(bundl);
+            return fragment;
+        } else if (position == 3){
+            bundl.putString("day","Thursday");
+            fragment.setArguments(bundl);
+            return fragment;
+        } else {
+            bundl.putString("day","Friday");
+            fragment.setArguments(bundl);
+            return fragment;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+}
